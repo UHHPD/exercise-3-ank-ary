1c9293df11c11843b595746634d7af2a5be8eb9e
Samuel Mcloughlin <samuelmcloughlin2248@gmail.com>
2025-11-05 17:07:30 +0100

    Q1c.

--- poisson.cc
+++ poisson.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 
 double poisson(double mu, int k) {
+    double P = pow(mu, k)*exp(-mu)/tgamma(k+1);
     return 0;
 }
 
@@ -13,8 +14,11 @@ int main() {
     
     ifstream fin("datensumme.txt");
     ofstream fout("hist.txt");
+    ofstream fout1("histpoi.txt");
 
     int n_i;
+    double mu = 3.11538;
+
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
         zaehler[n_i] +=1;
@@ -24,6 +28,7 @@ int main() {
     for(unsigned int k = 0 ; k < zaehler.size() ; ++k){
         cout << k << ":" << zaehler[k] << endl;
         fout << k << " " << zaehler[k] << endl;
+        fout1 << k << " " << zaehler[k] << zaehler[k]*poisson(mu,k) << endl;
     }
 
 }
